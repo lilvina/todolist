fca5c9625e8324c38e75e51a8916ba4df41a5efd
TJ Walker <tjfwalker+git@gmail.com>
2016-12-02 09:11:36 -0800

    Write backend routes
    API that returns JSON via /api/v1/...

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./routes/api/v1/users');
 
 var app = express();
 
@@ -23,8 +22,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/api/v1/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
